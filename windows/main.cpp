50a1ca6ff9da86cd32de5a13e846673f0286c1a0
wangyun <wangyun.ch7@gmail.com>
2016-11-17 10:01:51 +0800

    第14讲、窗口

--- windows/main.cpp
+++ windows/main.cpp
@@ -9,11 +9,40 @@
 #pragma comment(lib, "../Release/common.lib")
 #endif
 
+class ChangeWindow : public osgGA::GUIEventHandler
+{
+public:
+	ChangeWindow() {first = false;}
+	bool handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa)
+	{
+		if (!first)
+		{
+			osgViewer::Viewer * viewer = dynamic_cast<osgViewer::Viewer*> (&aa);
+			osgViewer::ViewerBase::Windows ws;
+			first = true;
+			viewer->getWindows(ws);
+			if (!ws.empty())
+			{
+				osgViewer::Viewer::Windows::iterator it = ws.begin();
+				for (; it!=ws.end(); it++)
+				{
+					(*it)->setWindowRectangle(50,50,800,600);
+					(*it)->setWindowDecoration(true);
+				}
+			}
+		}
+		return false;
+	}
+
+private:
+	bool first;
+};
 
 int main()
 {
 	osg::ref_ptr<osgViewer::Viewer> viewer = new osgViewer::Viewer;
 	viewer->setSceneData(osgDB::readNodeFile("glider.osg"));
+	viewer->addEventHandler(new ChangeWindow());
 	viewer->run();
 
 	return 0;
